0e1b828e01ec6dbfa7c6c23f9c02d68db9a885a4
mary <maryehllen.rosa@escola.pr.gov.br>
2025-04-08 18:04:12 -0300

    Update main.py

--- python/main.py
+++ python/main.py
@@ -28,3 +28,31 @@ def cadastrar_pessoa(cadastros):
     cadastros.append({"nome": nome, "idade": idade, "turma": turma, "curso": curso})
     salvar_cadastros(cadastros)
     print("Cadastro realizado com sucesso!")
+
+def ver_cadastros (cadastros):
+    if not cadastros:
+        print("\n Nenhum cadastro no sistema.")
+    else:
+        print("\n ------ LISTA DE CADASTROS ------")   
+        for i, pessoa in enumerate (cadastros, 1)
+            print(f"{i}. Nome:{pessoa['nome']}, Idade:{pessoa['idade']}, Turma: {pessoa['turma']}, Curso: {pessoa['curso']}")
+            input ("\nPressione Enter para voltar ao menu...")
+
+def main():
+    cadastros = carregar_cadastros()
+    while True:
+        exibir_menu()
+        opcao = input ("Escolha uma opção: ")
+        if opcao == "1":
+            cadastrar_pessoas(cadastros)
+        elif opcao == "2":
+            ver_cadastros(cadastros)
+        elif opcao == "3":
+            print ("Obrigada por utilizar o sistema de cadastro!")
+            break
+        else:
+            print("Opção invalida! Tente novamente.")
+
+if__name__ == "__main__":
+    main()
+        
